35ecd21862c111be2abb49b5614ddfe9527e7596
golery <noreply@mail.com>
2018-12-01 21:58:00 -0500

    Add link

--- main/src/canner/slate-editor-html/videoRules.js
+++ main/src/canner/slate-editor-html/videoRules.js
@@ -1,7 +1,8 @@
 // @flow
 import React from "react";
 import styled from "styled-components";
-import { nodeAttrs } from "@canner/slate-icon-shared";
+import {nodeAttrs} from "@canner/slate-icon-shared";
+import urlParser from "js-video-url-parser";
 
 export const VideoContiner = styled.iframe`
   src: ${props => props.src};
@@ -13,54 +14,63 @@ export const VideoContiner = styled.iframe`
     if (props.align === "center") return "center";
     else if (props.align === "right") return "flex-end";
     return "flex-start";
-  }};
+}};
 `;
 
 const defaultAttrs = {
-  id: node => node.data.get("id"),
-  ...nodeAttrs
+    id: node => node.data.get("id"),
+    ...nodeAttrs
 };
 
-export default function(inlineType = "video", stylesAttr = defaultAttrs) {
-  return {
-    deserialize(el) {
-      if (inlineType && el.tagName && el.tagName.toLowerCase() === "iframe") {
-        return {
-          object: "inline",
-          type: inlineType,
-          isVoid: true
-        };
-      }
-    },
-    serialize(obj) {
-      if (obj.object == "inline" && obj.type === inlineType) {
-        const align = stylesAttr.textAlign(obj);
-        const indent = stylesAttr.paddingLeft(obj);
-        const width = stylesAttr.width(obj) || 560;
-        const height = stylesAttr.height(obj) || 315;
-        const id = stylesAttr.id(obj);
-        let link;
+export default function (inlineType = "video", stylesAttr = defaultAttrs) {
+    return {
+        deserialize(el) {
+            if (inlineType && el.tagName && el.tagName.toLowerCase() === "iframe" && el.src) {
+                let src = el.src;
+                if (src.startsWith("https://www.youtube.com")) {
+                    let parse = urlParser.parse(src);
+                    if (parse && parse.id) {
+                        return {
+                            object: "inline",
+                            type: inlineType,
+                            isVoid: true,
+                            data: {
+                                id: parse.id
+                            }
+                        };
+                    }
+                }
+            }
+        },
+        serialize(obj) {
+            if (obj.object === "inline" && obj.type === inlineType) {
+                const align = stylesAttr.textAlign(obj);
+                const indent = stylesAttr.paddingLeft(obj);
+                const width = stylesAttr.width(obj) || 560;
+                const height = stylesAttr.height(obj) || 315;
+                const id = stylesAttr.id(obj);
+                let link;
 
-        if (inlineType === "youtube") {
-          link = `https://www.youtube.com/embed/${id}`;
-        } else if (inlineType === "dailymotion") {
-          link = `https://www.dailymotion.com/embed/video/${id}`;
-        } else if (inlineType === "vimeo") {
-          link = `https://player.vimeo.com/video/${id}`;
-        } else if (inlineType === "youku") {
-          link = `https://player.youku.com/embed/${id}`;
+                if (inlineType === "youtube") {
+                    link = `https://www.youtube.com/embed/${id}`;
+                } else if (inlineType === "dailymotion") {
+                    link = `https://www.dailymotion.com/embed/video/${id}`;
+                } else if (inlineType === "vimeo") {
+                    link = `https://player.vimeo.com/video/${id}`;
+                } else if (inlineType === "youku") {
+                    link = `https://player.youku.com/embed/${id}`;
+                }
+
+                return (
+                    <VideoContiner
+                        align={align}
+                        width={width}
+                        height={height}
+                        indent={indent}
+                        src={link}
+                    />
+                );
+            }
         }
-
-        return (
-          <VideoContiner
-            align={align}
-            width={width}
-            height={height}
-            indent={indent}
-            src={link}
-          />
-        );
-      }
-    }
-  };
+    };
 }
