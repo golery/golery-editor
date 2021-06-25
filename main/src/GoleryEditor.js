94a3045b3e0094fe52b03d09a36cf62b6764f449
hly <1429758+golery@users.noreply.github.com>
2021-06-25 17:41:50 -0400

    React

--- main/src/GoleryEditor.js
+++ main/src/GoleryEditor.js
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState, useCallback} from 'react'
+
+import {
+    Editor,
+    Transforms,
+    createEditor,
+    Descendant,
+    Element as SlateElement,
+} from 'slate'
+
+// Import the Slate components and React plugin.
+import {Slate, Editable, withReact} from 'slate-react'
 import {Editor as SlateEditor} from 'slate-react';
 import CodeBlockPlugin from "./plugins/codeblock/CodeBlockPlugin";
 import ImagePlugin from "./plugins/image/ImagePlugin";
@@ -8,6 +19,8 @@ import SoftBreakPlugin from "./plugins/softbreak/SoftBreakPlugin";
 import LinkPlugin from "./plugins/link/LinkPlugin";
 import VideoPlugin from "./plugins/video/VideoPlugin";
 import HeaderPlugin from "./plugins/header/HeaderPlugin";
+import {toggleBlock, toggleMark} from "./core/TextFormat";
+import {Element, Leaf} from "./core/Render";
 
 let imagePlugin = ImagePlugin();
 let codeBlockPlugin = CodeBlockPlugin();
@@ -22,17 +35,17 @@ let headerPlugin = HeaderPlugin();
 // https://github.com/Canner/canner-slate-editor/blob/master/packages/editors/canner-slate-editor/src/index.js
 // https://github.com/Canner/canner-slate-editor/blob/master/packages/editors/canner-slate-editor/src/menuToolbar.js
 let plugins = [
-    basicMarkPlugin,
-    editListPlugin,
-    codeBlockPlugin.slatePrism,
-    codeBlockPlugin.slateCodeBlock,
-    codeBlockPlugin,
-    imagePlugin,
-    listPlugin,
-    softBreakPlugin,
-    videoPlugin,
-    linkPlugin,
-    headerPlugin
+    // basicMarkPlugin,
+    // editListPlugin,
+    // codeBlockPlugin.slatePrism,
+    // codeBlockPlugin.slateCodeBlock,
+    // codeBlockPlugin,
+    // imagePlugin,
+    // listPlugin,
+    // softBreakPlugin,
+    // videoPlugin,
+    // linkPlugin,
+    // headerPlugin
 ];
 
 const schema = {
@@ -46,6 +59,8 @@ const schema = {
     },
 };
 
+
+
 /**
  * How to interact to GoleryEditor
  * <GoleryEditor controller=myController/>
@@ -58,62 +73,78 @@ const schema = {
  * Read: /work/golery-editor/main/src/sandbox/index.js for example usage
  *
  *  */
-class GoleryEditor extends React.Component {
-    // Set the initial value when the app is first constructed.
-    state = {
-        value: null,
-    };
+const GoleryEditor = (props) => {
+    const renderElement = useCallback(props => <Element {...props} />, [])
+    const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+    const editor = useMemo(() => withReact(createEditor()), []);
+   useEffect(()=>{
+           Object.assign(props.controller, {
+               toggleHeader: (level) => toggleBlock(editor, 'heading-one'),
+       //
+       //         toggleCode: () => this.editor.toggleCode("tsx"),
+       //         insertImage: (url) => this.editor.insertImage(url),
+       //
+               toggleBold: () => toggleMark(editor, 'bold'),
+               toggleUnderline: () => toggleMark(editor, 'underline'),
+               toggleItalic: () => toggleMark(editor, 'italic'),
+       //
+       //         toggleList: () => this.editor.toggleList(),
+       //         toggleBullet: () => this.editor.toggleBullet(),
+       //
+       //         isHeader: (level) => headerPlugin.isHeader(this.editor, level),
+       //         isInCodeBlock: () => codeBlockPlugin.isInCodeBlock(this.editor),
+       //         isInBold: () => basicMarkPlugin.isInBold(this.editor),
+       //         isInItalic: () =>  basicMarkPlugin.isInItalic(this.editor),
+       //         isInUnderline: () => basicMarkPlugin.isInUnderline(this.editor)
+           });
+   }, [props.controller]);
 
-    constructor(props) {
-        super(props);
-        Object.assign(props.controller, {
-            toggleHeader: (level) => this.editor.toggleHeader(level),
+    // }
 
-            toggleCode: () => this.editor.toggleCode("tsx"),
-            insertImage: (url) => this.editor.insertImage(url),
+    // return <SlateEditor value={this.state.value}
+    //                     onChange={this.onChange}
+    //                     plugins={plugins}
+    //                     ref={this.ref}
+    //                     schema={schema}
+    //                     {...this.props}
+    // />;
 
-            toggleBold: () => this.editor.toggleBold(),
-            toggleUnderline: () => this.editor.toggleUnderline(),
-            toggleItalic: () => this.editor.toggleItalic(),
+    // Add the initial value when setting up our state.
+    const [value, setValue] = useState([
+        {
+            type: 'paragraph',
+            children: [{text: 'A line of text in a paragraph.'}],
+        },
+    ])
 
-            toggleList: () => this.editor.toggleList(),
-            toggleBullet: () => this.editor.toggleBullet(),
+    console.log('Value', value);
 
-            isHeader: (level) => headerPlugin.isHeader(this.editor, level),
-            isInCodeBlock: () => codeBlockPlugin.isInCodeBlock(this.editor),
-            isInBold: () => basicMarkPlugin.isInBold(this.editor),
-            isInItalic: () =>  basicMarkPlugin.isInItalic(this.editor),
-            isInUnderline: () => basicMarkPlugin.isInUnderline(this.editor)
-        });
-    }
+    return (
+        <Slate
+            editor={editor}
+            value={value}
+            onChange={newValue => setValue(newValue)}
+        >
+            <Editable
+                renderElement={renderElement}
+                renderLeaf={renderLeaf}
+            />
+        </Slate>
+    );
 
-    // Render the editor.
-    render() {
-        return <SlateEditor value={this.state.value}
-                            onChange={this.onChange}
-                            plugins={plugins}
-                            ref={this.ref}
-                            schema={schema}
-                            {...this.props}
-        />;
-    }
-
-    onChange = ({value}) => {
-        this.setState({value})
-    };
-
-    ref = editor => {
-        this.editor = editor;
-        if (editor != null) {
-            editor.api = this.props.controller;
-        } else {
-            console.log('No editor');
-        }
-
-        // for debugging purpose
-        window.EDITOR = editor;
-        window.logValue = () => JSON.stringify(EDITOR.value, null, 2);
-    };
-}
+    //
+    // ref = editor => {
+    //     this.editor = editor;
+    //     if (editor != null) {
+    //         editor.api = this.props.controller;
+    //     } else {
+    //         console.log('No editor');
+    //     }
+    //
+    //     // for debugging purpose
+    //     window.EDITOR = editor;
+    //     window.logValue = () => JSON.stringify(EDITOR.value, null, 2);
+    // };
+};
 
 export default GoleryEditor;
