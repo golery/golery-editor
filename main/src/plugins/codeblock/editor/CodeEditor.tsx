0eacd2dfe84ea286f98f9f7e951235fd5fa11c2e
Golery <golery.team@gmail.com>
2021-07-23 23:40:52 -0400

    Syntax

--- main/src/plugins/codeblock/editor/CodeEditor.tsx
+++ main/src/plugins/codeblock/editor/CodeEditor.tsx
@@ -10,9 +10,10 @@ export const CodeEditor = ({onSave}) => {
     const [code, setCode] = useState('');
     const onClick = useCallback(() => {onSave(code)}, [code]);
     return (<div>
-        <textarea className={styles.code} value={code} onChange={e => setCode(e.target.value)}>This is code</textarea>
+        <textarea className={styles.code} value={code} onChange={e => setCode(e.target.value)}
+                    placeholder={'Paste code here'}/>
         <DialogFooter>
-            <button onClick={onClick}>Save</button>
+            <button onClick={onClick}>SAVE</button>
         </DialogFooter>
     </div>);
 }
