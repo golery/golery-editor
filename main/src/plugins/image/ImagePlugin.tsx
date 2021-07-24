01aa66fd306feacecf2bf872a19efb87c5778717
Golery <golery.team@gmail.com>
2021-07-24 10:33:47 -0400

    Syntax

--- main/src/plugins/image/ImagePlugin.tsx
+++ main/src/plugins/image/ImagePlugin.tsx
@@ -3,15 +3,15 @@ import {WidgetRenderParams} from "../../core/EditorTypes";
 import {EditorPlugin} from "../../core/EditorPlugin";
 
 export const ImagePlugin:EditorPlugin = {
-    // id: 'image',
-    // elmType: 'img',
+    id: 'image',
+    type: 'image',
     // name: 'Image',
     // icon: 'image',
-    // async getDataWhenInsert() {
-    //     return Promise.resolve({url: 'https://picsum.photos/200/300'});
-    // },
     init() {},
-    render({type, data}: WidgetRenderParams) {
-        if (type === 'img') return <img src={data.src} alt={data.src}/>;
+    render({type, data, attributes, children}: WidgetRenderParams) {
+        if (type === 'image') return <div  {...attributes}>{children}<img src={data.url} alt={data.url}/></div>;
     },
+    async onInsert() {
+        return ({url: 'https://picsum.photos/200/300'});
+    }
 }
